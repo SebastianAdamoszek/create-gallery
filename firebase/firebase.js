eb09363b3eb27195ebcbf94440392ea368284b73
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-10-27 13:38:29 +0100

    add .env

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,27 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-//  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-//  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-
- apiKey: "AIzaSyCvwHDOhReMqeY5NBvCsbA3foEiGOvUYnU",
-
-  authDomain: "create-gallery.firebaseapp.com",
-
-  projectId: "create-gallery",
-
-  storageBucket: "create-gallery.appspot.com",
-
-  messagingSenderId: "683983058698",
-
-  appId: "1:683983058698:web:c571ef94bcdf11eaf3acf3",
-
-  measurementId: "G-L9NHQ546Q8",
+ apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+ authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+ projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+ storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+ messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+ appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+ measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inicjalizacja Firebase
