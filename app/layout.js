80a988b3df9e039085898822ec7e5c12ab9e7559
unknown <sebaadamoszek@vp.pl>
2024-11-12 20:01:07 +0100

    pl lang

--- app/layout.js
+++ app/layout.js
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
 
   return (
     <>
-      <html lang="en">
+      <html lang="pl">
         <body className={inter.className}>
           {loading ? (
             <Loader />
