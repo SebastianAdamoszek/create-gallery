54241b530d3b81a756823a9c9dca9cbf36ee2411
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-11-24 01:49:07 +0100

    change page user-gallery

--- app/user-gallery/[userId]/page.js
+++ app/user-gallery/[userId]/page.js
@@ -1,159 +1,68 @@
 "use client";
-import styles from "@/app/page.module.css";
 import { use, useEffect, useState } from "react";
 import { db } from "@/firebase/firebase"; // Dostęp do Firestore
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 import {
   collection,
   doc,
   getDoc,
-  getDocs,
   query,
   orderBy,
   onSnapshot,
-  deleteDoc,
 } from "firebase/firestore";
-import { Photo, PhotoForDel } from "@/components/ForAdmin/Photo/Photo";
+import { PhotoPublic } from "@/components/Gallery/PhotoPublic/PhotoPublic";
 import {
   GalleryPageContainer,
   GalleryContainer,
-} from "@/components/ForAdmin/ForAdminUsersGalleries/ForAdminUsersGalleries.styled";
-// import { ButtonsContainer } from "@/components/ForAdmin/ButtonsAddDelPhoto/ButtonsAddDelPhoto.styled";
-// import {
-//   ButtonAddPhoto,
-//   ButtonDelPhoto,
-// } from "@/components/ForAdmin/ButtonsAddDelPhoto/ButtonsAddDelPhoto";
-import { useRouter } from "next/navigation"; // Używamy useRouter do przekierowania
+} from "@/components/Gallery/Gallery.styled";
+import styles from "../../page.module.css";
+import "@/app/globals.css";
 
-const UserGalleryAdminPage = ({ params }) => {
-  const { userId } = params; // Wyciąganie userId z params
+const UserGalleryPage = ({ params }) => {
+  const { userId } = use(params); // Get userId from URL params
   const [photos, setPhotos] = useState([]);
-  const [userEmail, setUserEmail] = useState("");
-  const [isDeleteMode, setIsDeleteMode] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const router = useRouter();
+  const [userEmail, setUserEmail] = useState(""); // Stan do przechowywania e-maila użytkownika
 
-  const fetchPhotos = async (userId) => {
-    const querySnapshot = await getDocs(
-      collection(db, `galleries/${userId}/photos`)
+  useEffect(() => {
+    // Query to fetch photos for the specific userId
+    const q = query(
+      collection(db, `galleries/${userId}/photos`),
+      orderBy("timestamp", "desc")
     );
-    const photos = [];
-    querySnapshot.forEach((doc) => {
-      photos.push({ id: doc.id, ...doc.data() });
-    });
-    return photos;
-  };
-
-  useEffect(() => {
-    const auth = getAuth();
-
-    const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-        router.push("/home");
-      }
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setPhotos(snapshot.docs.map((doc) => doc.data()));
     });
 
-    return () => unsubscribeAuth();
-  });
-
-  useEffect(() => {
-    const loadPhotos = async () => {
-      try {
-        const fetchedPhotos = await fetchPhotos(userId);
-        setPhotos(fetchedPhotos);
-      } catch (error) {
-        console.error("Błąd podczas pobierania zdjęć:", error);
-      }
-    };
-
-    loadPhotos();
+    return () => unsubscribe(); // Clean up subscription on unmount
   }, [userId]);
 
-  const toggleDeleteMode = () => {
-    setIsDeleteMode(!isDeleteMode);
-  };
-
-  const handleAddPhoto = () => {
-    setShowModal(true);
-  };
-
   useEffect(() => {
+    // Funkcja do pobrania e-maila użytkownika na podstawie userId
     const fetchUserEmail = async () => {
-      const docRef = doc(db, "users", userId);
-      const docSnap = await getDoc(docRef);
+      const docRef = doc(db, "users", userId); // Odwołanie do dokumentu użytkownika w Firestore
+      const docSnap = await getDoc(docRef); // Pobierz dokument
 
       if (docSnap.exists()) {
-        setUserEmail(docSnap.data().email);
+        setUserEmail(docSnap.data().email); // Ustaw e-mail w stanie
       } else {
         console.log("Brak dokumentu użytkownika");
       }
     };
 
-    fetchUserEmail();
-  }, [userId]);
-
-  const handleDeletePhoto = async (photoId) => {
-    try {
-      await deleteDoc(doc(db, `galleries/${userId}/photos`, photoId));
-      setPhotos(photos.filter((photo) => photo.id !== photoId));
-    } catch (error) {
-      console.error("Błąd podczas usuwania zdjęcia:", error);
-    }
-  };
-
-  useEffect(() => {
-    const loadPhotos = () => {
-      const photosRef = collection(db, `galleries/${userId}/photos`);
-      const q = query(photosRef, orderBy("timestamp", "desc"));
-
-      const unsubscribe = onSnapshot(q, (snapshot) => {
-        const photos = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setPhotos(photos);
-      });
-
-      return () => unsubscribe();
-    };
-
-    loadPhotos();
+    fetchUserEmail(); // Wywołanie funkcji
   }, [userId]);
 
   return (
     <div className={styles.main__next}>
       <GalleryPageContainer>
-        <h2>Galeria użytkownika: {userEmail}</h2>
-        {/* <ButtonsContainer>
-          <ButtonDelPhoto
-            toggleDeleteMode={toggleDeleteMode}
-            isDeleteMode={isDeleteMode}
-            userId={userId}
-          />
-          <ButtonAddPhoto onAddPhoto={handleAddPhoto} userId={userId} />
-        </ButtonsContainer> */}
-
+        <h2>Galeria użytkownika:</h2>
+        <h3 className={`${!userEmail ? "loading-text" : ""}`}>
+          {userEmail || "Pobieram..."}
+        </h3>
         <GalleryContainer>
-          {photos.map((photo) =>
-            isDeleteMode ? (
-              <PhotoForDel
-                key={photo.id}
-                url={photo.url}
-                photoId={photo.id}
-                onDeletePhoto={handleDeletePhoto}
-                userId={userId}
-              />
-            ) : (
-              <Photo
-                key={photo.id}
-                url={photo.url}
-                userId={userId}
-                docId={photo.id}
-              />
-            )
+          {photos.length > 0 ? (
+            photos.map((photo, index) => <PhotoPublic key={index} url={photo.url} />)
+          ) : (
+            <p>Brak zdjęć do wyświetlenia.</p>
           )}
         </GalleryContainer>
       </GalleryPageContainer>
@@ -161,4 +70,4 @@ const UserGalleryAdminPage = ({ params }) => {
   );
 };
 
-export default UserGalleryAdminPage;
+export default UserGalleryPage;
