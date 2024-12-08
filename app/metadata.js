dcd951d8b46714a00376939e62b47a4d66f8a128
SebastianAdamoszek <sebaadamoszek@vp.pl>
2024-12-08 18:59:56 +0100

    meta data head html

--- app/metadata.js
+++ app/metadata.js
@@ -1,11 +1,11 @@
 export const metadata = {
   title: "Serwis zegarmistrzowski",
-  description: "Profesjonalne usługi zegarmistrzowskie. Naprawa, konserwacja, i serwis zegarków na najwyższym poziomie.",
+  description: "Kompleksowe usługi zegarmistrzowskie. Naprawa, konserwacja, i serwis zegarków na najwyższym poziomie.",
   keywords: "zegarmistrz, naprawa zegarków, serwis zegarków, zegary",
   openGraph: {
     title: "Serwis zegarmistrzowski",
     description: "Najlepsze usługi zegarmistrzowskie w Twojej okolicy!",
-    images: ["/path-to-your-image.jpg"],
+    images: ["/dial2.jpg"],
   },
   viewport: "width=device-width, initial-scale=1",
   icons: {
